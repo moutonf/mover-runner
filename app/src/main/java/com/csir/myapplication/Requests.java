dfedae396bfca57e56f484b9779134349edd225b
Gavin Wiener <gavinwiener@gmail.com>
2016-07-11 12:10:10 +0200

    OkHTTP used for connecitons

--- app/src/main/java/com/csir/myapplication/Requests.java
+++ app/src/main/java/com/csir/myapplication/Requests.java
@@ -8,19 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.BufferedInputStream;
+/*OkHTTP classes*/
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Requests extends AppCompatActivity {
 
     TextView txtView;
-    Button button;
+    Button button, postBtn;
+    OkHttpClient client;
 
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private static final String TAG = "CONNECTION";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,46 +37,57 @@ public class Requests extends AppCompatActivity {
         txtView = (TextView) findViewById(R.id.textView);
         txtView.setText("Waiting for response..");
 
-        button = (Button) findViewById(R.id.button_id);
+        button = (Button) findViewById(R.id.get_button);
+        postBtn = (Button) findViewById(R.id.post_button);
 
-        /*Async tasks and threads should be used for connections and requests*/
+        client  = new OkHttpClient();
+        /*Requests should be async, this is dirty*/
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
     }
 
-    public void load(View view) {
-        //Testing internet connection
-        HttpURLConnection urlConnection = null;
-        URL url;
+
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+    public void getRequest(View view) {
+        String response;
         try {
-
-            url = new URL("http://moutonf.co.za:5000/test-api");
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.connect();
-            int code = urlConnection.getResponseCode();
-            Log.i(TAG, "Response code=" + code);
-//            txtView.setText("Response code: " + code);
-
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            InputStreamReader isw = new InputStreamReader(in);
-            String response = "";
-            int data = isw.read();
-            while (data != -1) {
-                char current = (char) data;
-                data = isw.read();
-                response += current;
-            }
+            response = run("http://moutonf.co.za:5000/get-api");
             txtView.append(response);
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public void postRequest(View view) {
+        String response;
+        try {
+        //MUST BUILD A JSON
+         response=  post("http://moutonf.co.za:5000/post-api","");
 
         }catch(IOException ex){
             ex.printStackTrace();
         }
-        finally {
-            if (urlConnection !=null){
-                urlConnection.disconnect();
 
-            }
-
-        }
     }
+
+
 }
