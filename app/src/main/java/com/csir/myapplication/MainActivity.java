8788deaf2dba6cc9e5b5d6135db64acfd3e79f81
Gavin Wiener <gavinwiener@gmail.com>
2016-08-04 19:25:15 +0200

    Need to add server request and account registration

--- app/src/main/java/com/csir/myapplication/MainActivity.java
+++ app/src/main/java/com/csir/myapplication/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(LoginActivity.USER_ID);
+        this.setTitle("User:" + message);
     }
 
     public void getRunner(View view) {
