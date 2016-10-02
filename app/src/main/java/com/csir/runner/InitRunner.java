a1b7925af389680b799babeb05302b5b4993416f
Gavin Wiener <gavin.wiener@brandseye.com>
2016-10-02 18:45:18 +0200

    WakeLock on application. Doesn't work

---
+++ app/src/main/java/com/csir/runner/InitRunner.java
@@ -0,0 +1,37 @@
+package com.csir.runner;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+
+/**
+ * Created by brandseye-gavin on 2016/10/02.
+ */
+
+public class InitRunner extends Application {
+
+    private static final String TAG = "RUNNER_INITIALIZE";
+
+    PowerManager.WakeLock mWakeLock;
+    private static InitRunner singleton = null;
+
+    public static InitRunner getInstance() {
+        return singleton;
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate() {
+        singleton = this;
+        super.onCreate();
+
+        /* This code together with the one in onDestroy()
+         * will make the screen be always on until this Activity gets destroyed. */
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        this.mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Application wakelock");
+        /*wakelock will be released when the process is killed, very bad coding practice. hack fix*/
+        this.mWakeLock.acquire();
+    }
+
+}
